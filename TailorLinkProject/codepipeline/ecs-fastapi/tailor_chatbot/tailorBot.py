7d96149267e72a136d3550fd7e159b70826166bc
SKN_03_JiyongPark <a85429462@gmail.com>
2024-11-20 17:44:13 +0900

    feat init file

--- TailorLinkProject/codepipeline/ecs-fastapi/tailor_chatbot/tailorBot.py
+++ TailorLinkProject/codepipeline/ecs-fastapi/tailor_chatbot/tailorBot.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello, world!"}
+@app.get("")
+def read_root():
+    return {"message": "Hello, World!"}
+
+@app.get("/_stcore/health")
+def health_check():
+    return {"status": "healthy"}
